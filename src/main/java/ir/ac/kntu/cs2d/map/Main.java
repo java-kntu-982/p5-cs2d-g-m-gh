28e895e60c97a3dcd1b4c8cc4c4ad842977d7e56
ghazal-pouresfandiyar <ghazal.brn@gmail.com>
2020-07-24 00:56:06 +0430

    "edit reading map"

--- src/main/java/ir/ac/kntu/cs2d/map/Main.java
+++ src/main/java/ir/ac/kntu/cs2d/map/Main.java
@@ -9,32 +9,57 @@ import javafx.stage.Stage;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends Application {
-
     public void start(Stage primaryStage) {
         Pane root = new Pane();
         FileReader inputStream = null;
         try {
             inputStream = new FileReader("map.txt");
-            int c,i=1;
+            int c,i=1,j=1,counter=0;
+            List<Rectangle> rectangleList=new ArrayList<>();
             while ((c = inputStream.read()) != -1) {
-                if(c==1) {
-                    Rectangle rectangle = new Rectangle(1, 1);
-                    rectangle.setFill(Color.RED);
-                    rectangle.setX(i);
-                    rectangle.setY(i);
-                    i++;
-                    root.getChildren().add(rectangle);
+                System.out.println("1");
+                rectangleList.add(new Rectangle(1, 1));
+                rectangleList.get(counter).setX(i);
+                rectangleList.get(counter).setY(j);
+                i++;
+                switch (c){
+                    case 0:
+                        System.out.println("2");
+                        rectangleList.get(counter).setFill(Color.MINTCREAM);
+                        break;
+                    case 1:
+                        rectangleList.get(counter).setFill(Color.WHITE);
+                        break;
+                    case 2:
+                        rectangleList.get(counter).setFill(Color.BROWN);
+                        break;
+                    case 3:
+                        rectangleList.get(counter).setFill(Color.ORANGE);
+                        break;
+                    case 4:
+                        rectangleList.get(counter).setFill(Color.RED);
+                        break;
+                    case 5:
+                        rectangleList.get(counter).setFill(Color.GRAY);
+                        break;
+                    default:
+                        rectangleList.get(counter).setFill(Color.YELLOW);
+                        break;
                 }
+                System.out.println("3");
+                root.getChildren().add(rectangleList.get(counter));
+                counter++;
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Scene scene = new Scene(root, 800, 800, Color.WHITE);
-
+        Scene scene = new Scene(root, 1350, 800, Color.BLACK);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
