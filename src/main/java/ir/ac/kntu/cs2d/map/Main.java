9161e63151409438ccac48eeeecd8156032d2ba6
ghazal-pouresfandiyar <ghazal.brn@gmail.com>
2020-07-24 00:37:03 +0430

    "read map"

--- src/main/java/ir/ac/kntu/cs2d/map/Main.java
+++ src/main/java/ir/ac/kntu/cs2d/map/Main.java
@@ -1,18 +1,46 @@
 package ir.ac.kntu.cs2d.map;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
-import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main extends Application {
 
+    public void start(Stage primaryStage) {
+        Pane root = new Pane();
+        FileReader inputStream = null;
+        try {
+            inputStream = new FileReader("map.txt");
+            int c,i=1;
+            while ((c = inputStream.read()) != -1) {
+                //TODO
+                if(c==1) {
+                    Rectangle rectangle = new Rectangle(1, 1);
+                    rectangle.setFill(Color.RED);
+                    rectangle.setX(i);
+                    rectangle.setY(i);
+                    i++;
+                    root.getChildren().add(rectangle);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(root, 800, 800, Color.WHITE);
 
-    @Override
-    public void start(Stage stage) throws Exception {
-
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+            launch(args);
     }
 }
